4a02a7a447822018be391f8d8aff9adf7dccd758
jan248 <jan.skoberne8@gmail.com>
2020-10-11 22:28:34 +0200

    dodani komentarji

--- model.py
+++ model.py
@@ -1,12 +1,12 @@
 import json
-import datetime as dt
+import datetime as dt # uvozim knjižnico za efektivno delo s časom v pythonu
 from bottle import *
-import os
+import os # operacijski sistem za uporabo funkcije ROOT
 
-kodiranje = 'skrivnost'
+kodiranje = 'skrivnost' # za uporabo piškotkov v id_uporabnika
 ROOT = os.environ.get('BOTTLE_ROOT', '/')
 
-
+# uporaba relativne poti
 def rtemplate(*largs, **kwargs):
     """
     Izpis predloge s podajanjem spremenljivke ROOT z osnovnim URL-jem.
@@ -15,7 +15,7 @@ def rtemplate(*largs, **kwargs):
 
 
 
-
+# funkcija za preverjanje ali je uporabnik že prijavljen in uporabo piškotkov
 def id_uporabnik():
     if request.get_cookie("id", secret = kodiranje):
         piskotek = request.get_cookie("id", secret = kodiranje)
@@ -23,65 +23,73 @@ def id_uporabnik():
     else:
         return 0
 
-
+# pomožne funkcije za delo z jsoni
+# funkcija odpre datoteko
 def odpri_json(pot):
     with open(pot, 'r') as f:
         return json.load(f)
 
+# funkcija zapiše v datoteko
 def zapisi_json(pot, podatki):
     with open(pot, 'w') as f:
         json.dump(podatki, f)
 
+# funkcija doda podatke in jih zapiše v json
 def dodaj_in_zapisi(pot, podatek):
     data = odpri_json(pot)
     data.append(podatek)
     zapisi_json(pot, data)
 
-
+# funkcija poišče zmagovalca, njegovo ceno in id
 def najdi_zmagovalca(id):
-    data = odpri_json('podatki/stave.json')
+    data = odpri_json('podatki/stave.json') # odpre vse stave
     seznam = []
+    # naredimo seznam vseh stav narejenih na določeno ponudbo
     for i in data:
         if int(i['id_ponudbe']) == int(id):
             helper =  [i['cena'], i['id_ponudnika'], i['ime']]
             seznam.append(helper)
     cene = []
     if seznam == []:
-        return 0,0,'Nihče'
+        return 0,0,'Nihče' # ni zmagovalca
     for i in seznam:
         cene.append(i[0])
     cena = max(cene)
-    stevilo = cene.index(cena)
+    stevilo = cene.index(cena) # na katerem  mestu se nahaja maksimalna ponujena cena
     zmagovalec = seznam[stevilo][1]
     ime = seznam[stevilo][2]
     return zmagovalec, cena, ime
     
-
+# strtime preformatira string v datetime objekt
+# funkcija odpre vse podatke o ponudbah in pregleda njihovo veljavnost
 def preglej_stare():
     podatki = odpri_json('podatki/oglasi.json')
     seznam = []
     for i in podatki:
-        if dt.datetime.strptime(i['zakljucek'], '%d.%m.%Y %H:%M') < dt.datetime.now():
+        if dt.datetime.strptime(i['zakljucek'], '%d.%m.%Y %H:%M') < dt.datetime.now(): # pregled če je dražba že potekla
             zmagovalec, cena, ime = najdi_zmagovalca(i["id_ponudbe"])
-            podatek = {"id_ponudbe": i["id_ponudbe"], "id_zmagovalca": zmagovalec, "cena": cena, "id_ponudnika":i['prodajalec_id'], 'ime_zmagovalca':ime}
+            podatek = {"id_ponudbe": i["id_ponudbe"], "id_zmagovalca": zmagovalec, "cena": cena, "id_ponudnika":i['prodajalec_id'], 'ime_zmagovalca':ime} # slovar zaključenih ogalsov
             dodaj_in_zapisi('podatki/stari_oglasi.json', podatek)
         else:
-            seznam.append(i)
+            seznam.append(i) # seznam oglasov, ki še niso pretekli
     zapisi_json('podatki/oglasi.json', seznam)
 
+# funkcija za preverbo uporabnika
 def preveri_uporabnika(ime, geslo):
-    uporabniki = odpri_json('podatki/uporabniki.json')
+    uporabniki = odpri_json('podatki/uporabniki.json') # odpremo vse podatke o uporabnikih
     for uporabnik in uporabniki:
-        if uporabnik['uid'] == ime and uporabnik['geslo'] == geslo:
+        if uporabnik['uid'] == ime and uporabnik['geslo'] == geslo: # preveri če se ime in geslo ujemata
             return True
     return False
 
+# funkcija za pridobivanje podatkov uporabniškega imena
 def pridobi_podatke(uid):
     uporabniki = odpri_json('podatki/uporabniki.json')
     for uporabnik in uporabniki:
         if uporabnik['uid'] == uid:
             return uporabnik
 
+# funkcija za pridobivanje podatkov id uporabnikov
 def uporabnik_podatki(no):
     no = int(no)
     uporabniki = odpri_json('podatki/uporabniki.json')
@@ -89,6 +97,7 @@ def uporabnik_podatki(no):
         if uporabnik['id'] == no:
             return uporabnik
 
+# funkcija za iskanje nove številke za id uporabnika 
 def vstavi_novega(ime, priimek, uid, geslo):
     data = odpri_json('podatki/uporabniki.json')
     seznam = []
@@ -96,7 +105,7 @@ def vstavi_novega(ime, priimek, uid, geslo):
         seznam.append(i['id'])
     try: 
         naslednja_stevilka = max(seznam) + 1
-    except: naslednja_stevilka = 1
+    except: naslednja_stevilka = 1 # v primeru registracije prvega uporabnika
 
     dodaj_in_zapisi('podatki/uporabniki.json', {'id':naslednja_stevilka, 'ime':ime, 'priimek': priimek, 'uid': uid, 'geslo':geslo})
     return naslednja_stevilka
--- vmesnik.py
+++ vmesnik.py
@@ -31,13 +31,13 @@ TEMPLATE_PATH.insert(0, abs_views_path )
 static_dir = "./static"
 
 
-
+# pot za statične vire, torej slike in css
 @route("/static/<filename:path>")
 def static(filename):
     return static_file(filename, root=static_dir)
 
 
-
+# naredimo začetno stran
 @get('/')
 def index():
     preglej_stare()
@@ -49,19 +49,20 @@ def zacetna():
     preglej_stare()
     redirect('{0}'.format(ROOT))
 
-
+# funkcija za prijavo uporabnika
 @get('/prijava/')
 def prijava():
-    preglej_stare()
-    stanje = id_uporabnik()
+    preglej_stare() # pregled vseh obstoječih uporabnikov
+    stanje = id_uporabnik() # stanje nastavi na id uporabnika
     return rtemplate('prijava.html', napaka = 0, stanje = stanje)
 
+# funkcija, ki preusmeri na uporabnika če je ta veljaven in vrne napako če ni
 @post('/prijava/')
 def prijavljanje():
     uid = request.forms.uid
     geslo = request.forms.geslo
 
-    if preveri_uporabnika(uid, geslo):
+    if preveri_uporabnika(uid, geslo): # preverimo če je uporabnik veljaven
         uporabnik = pridobi_podatke(uid)
         stevilka = uporabnik['id']
         response.set_cookie("id",stevilka, path='/', secret = kodiranje)
@@ -70,7 +71,7 @@ def prijavljanje():
         return rtemplate('prijava.html', stanje = 0, napaka = 1)
 
 
-
+# pridobimo podatke o uporabniku in nas pelje na stran uporabnika
 @get('/uporabnik/')
 def uporabnik():
     preglej_stare()
@@ -81,24 +82,24 @@ def uporabnik():
     return rtemplate('uporabnik.html',stanje = stanje, ime = ime, priimek = priimek)
 
 
-
+# funkcija za pridobitev podatkov za registracijo
 @get('/registracija/')
 def registracija():
     preglej_stare()
     stanje = id_uporabnik()
-    polja_registracija = ("ime", "priimek", "uid", "pass1", "pass2")
+    polja_registracija = ("ime", "priimek", "uid", "pass1", "pass2") # ta slovar uporabimo za registracijo in nam v primeru napačne registracije ni potrebno pisati še vse ostalo razen obeh gesel
     podatki = {polje: "" for polje in polja_registracija} 
     return rtemplate('registracija.html', napaka = 0,stanje = stanje, **podatki)
 
 
 
-
+# funkcija za izpolnitev registracije
 @post('/registracija/')
 def registriranje():
     stanje = id_uporabnik()
     polja_registracija = ("ime", "priimek", "uid", "pass1", "pass2")
     podatki = {polje: "" for polje in polja_registracija}
-    podatki = {polje: getattr(request.forms, polje) for polje in polja_registracija}
+    podatki = {polje: getattr(request.forms, polje) for polje in polja_registracija} # na podatkih zapolni slovar z atributi
 
     ime = podatki.get('ime')
     priimek = podatki.get('priimek')
@@ -106,18 +107,18 @@ def registriranje():
     geslo1 = podatki.get('pass1')
     geslo2 = podatki.get('pass2')
 
-    if ime == '' or priimek == '' or uid == '' or geslo1 == '' or geslo2 == '':
+    if ime == '' or priimek == '' or uid == '' or geslo1 == '' or geslo2 == '': # vrne napako če je kakšno polje prazno
         return rtemplate('registracija.html', napaka = 1, stanje = stanje, **podatki)
 
     data = odpri_json('podatki/uporabniki.json')
     for i in data:
         if i['uid'] == uid:
-            return rtemplate('registracija.html', napaka = 2,stanje = stanje, **podatki)
+            return rtemplate('registracija.html', napaka = 2,stanje = stanje, **podatki) # vrne napako če je uporabniško ime že zasedeno
 
 
-    if len(geslo1) < 6:
+    if len(geslo1) < 6: # vrne napako če je geslo prekratko
         return rtemplate('registracija.html', napaka =5,stanje = stanje,  **podatki)
-    if geslo1 == geslo2:
+    if geslo1 == geslo2: # če se gesli ujemata in je ostalo veljavno preusmeri na stran uporabnika
         uid = vstavi_novega(ime, priimek, uid, geslo1)
         response.set_cookie("id",uid, path='/', secret = kodiranje)
         string = '{0}uporabnik/'.format(ROOT)
@@ -125,6 +126,7 @@ def registriranje():
     else:
         return rtemplate('registracija.html', stanje = stanje, napaka = 4, **podatki)
 
+# ob odjavi uporabnika se zbrišejo piškotki in vrnitev na začetno stran
 @get('/odjava/')
 def odjava():
     preglej_stare()
@@ -135,7 +137,7 @@ def odjava():
 
 
 
-
+# odpremo podatke o oglasih in iz seznama oglasov dobimo seznam seznamov oglasov
 @get('/oglasi/')
 def oglasi():
     stanje = id_uporabnik()
@@ -149,6 +151,7 @@ def oglasi():
         oglasi.append(helper)
     return rtemplate('oglasi.html', stanje = stanje, oglasi = oglasi)
 
+# funkcija za pregled oglasov in izračun časa 
 @get('/oglas/<oznaka>')
 def oglas(oznaka):
     stanje = id_uporabnik()
@@ -158,9 +161,9 @@ def oglas(oznaka):
         if int(i['id']) == int(oznaka):
             podatki = i    
 
-    cas_do_konca = str(dt.datetime.strptime(podatki['zakljucek'], '%d.%m.%Y %H:%M') - dt.datetime.now())
+    cas_do_konca = str(dt.datetime.strptime(podatki['zakljucek'], '%d.%m.%Y %H:%M') - dt.datetime.now()) # izračun časa do konca veljavnosti dražbe oglasa
     i = cas_do_konca.find('days')
-    pretvorba = cas_do_konca[:i] + 'dni' + cas_do_konca[i+4:]
+    pretvorba = cas_do_konca[:i] + 'dni' + cas_do_konca[i+4:] # pretvorba v lepši način izpisa časa do konca veljavnosti dražbe
     cas_do_konca = ':'.join(str(pretvorba).split(':')[:2])
 
     podatk = odpri_json('podatki/stave.json')
@@ -176,7 +179,7 @@ def oglas(oznaka):
 
 
 
-
+# funkcija vrne oglase uporabnika
 @get('/uporabnik/oglasi')
 def oglasi_uporabnika():
     stanje = id_uporabnik()
@@ -184,7 +187,7 @@ def oglasi_uporabnika():
     data = odpri_json('podatki/oglasi.json')
     oglasi = []
     for i in data:
-        if i["prodajalec_id"] == stanje:
+        if i["prodajalec_id"] == stanje: # če se prodajalec ujema s stanjem zapišemo vse njegove oglase
             pomozni = []
             pomozni.append(i['id'])
             pomozni.append(i['ime'])
@@ -194,6 +197,7 @@ def oglasi_uporabnika():
             oglasi.append(pomozni)
     return rtemplate('oglasi_uporabnik.html', oglasi = oglasi, stanje = stanje)
 
+# funkcija za odstranitev oglasa določenega uporabnika
 @post('/uporabnik/oglasi/<oznaka>')
 def odstrani(oznaka):
     data = odpri_json('podatki/oglasi.json')
@@ -202,10 +206,10 @@ def odstrani(oznaka):
         if int(i['id']) != int(oznaka):
             oglasi.append(i)
     zapisi_json('podatki/oglasi.json', oglasi)
-    redirect('{0}uporabnik/oglasi'.format(ROOT))
+    redirect('{0}uporabnik/oglasi'.format(ROOT)) # preusmeritev na oglase uporabnika
 
 
-
+# najprej pridobimo podatke o oglasu in dodajanje ponudb na oglas
 @post('/oglas/<oznaka>')
 def stavi(oznaka):
     stanje = id_uporabnik()
@@ -234,17 +238,17 @@ def stavi(oznaka):
     try:
         cena = int(request.forms.cena)
     except:
-        return rtemplate('oglas.html', stanje = stanje, konec = cas_do_konca, napaka = 1, ostali = seznam, **podatki)
+        return rtemplate('oglas.html', stanje = stanje, konec = cas_do_konca, napaka = 1, ostali = seznam, **podatki) # vrne napako če cena ni številka
 
     if cena < int(podatki['zacetna_cena']):
-        return rtemplate('oglas.html', stanje = stanje, konec = cas_do_konca, napaka = 2, ostali = seznam, **podatki)
+        return rtemplate('oglas.html', stanje = stanje, konec = cas_do_konca, napaka = 2, ostali = seznam, **podatki) # vrne napako ker je cena prenizka
 
     stava = {'id_ponudbe':podatki['id'], 'id_ponudnika': stanje, 'cena':cena, "ime": uporabnik['ime'] + ' ' + uporabnik["priimek"]}
     dodaj_in_zapisi('podatki/stave.json', stava)
     redirect('{0}oglas/{1}'.format(ROOT, oznaka))
 
     
-
+# funkcija za pregled ponudb uporabnika
 @get('/uporabnik/ponudbe')
 def ponudbe():
     stanje = id_uporabnik()
@@ -252,7 +256,7 @@ def ponudbe():
     podatki = odpri_json('podatki/stave.json')
     seznam = []
     for i in podatki:
-        if int(i['id_ponudnika']) == int(stanje):
+        if int(i['id_ponudnika']) == int(stanje): # doda ponudbe uporabnika če se stanje in id uporabnika ujemata
             helper = []
             helper.append(i['id_ponudbe'])
             helper.append(i['cena'])
@@ -260,7 +264,7 @@ def ponudbe():
     return rtemplate('ponudbe.html', stanje = stanje, ponudbe = seznam)
 
 
-
+# funkcija za pregled končanih dražb
 @get('/uporabnik/koncane')
 def koncane():
     preglej_stare()
@@ -269,21 +273,21 @@ def koncane():
     seznam_tvojih = []
     seznam_zmag = []
     for i in podatki:
-        if int(i['id_ponudnika']) == int(stanje):
+        if int(i['id_ponudnika']) == int(stanje): # dražba na kateri je uporabnik dal neko ponudbo se je zaključila
             helper = []
             for key in i:
                 helper.append(i[key])
             seznam_tvojih.append(helper)
-        elif int(i['id_zmagovalca']) == int(stanje):
+        elif int(i['id_zmagovalca']) == int(stanje): # vrne zmago na dražbi
             helper = []
             for key in i:
                 helper.append(i[key])
             seznam_zmag.append(helper)
-    return rtemplate('koncane.html', moje = seznam_tvojih, stanje = stanje, zmage = seznam_zmag)
+    return rtemplate('koncane.html', moje = seznam_tvojih, stanje = stanje, zmage = seznam_zmag) # naredi seznam zmag uporabnika in seznam njegovih zaključenih dražb
 
 
 
-
+# funkcija za pridobitev podatkov o dodajanju oglasa
 @get('/uporabnik/dodaj')
 def dodaj_oglas():
     stanje = id_uporabnik()
@@ -292,7 +296,7 @@ def dodaj_oglas():
 
 
 
-
+# funkcija, ki zapiše nov oglas uporabnika
 @post('/uporabnik/dodaj')
 def dodaj():
     stanje = id_uporabnik()
@@ -300,19 +304,19 @@ def dodaj():
     opis = request.forms.opis
     cena = request.forms.cena 
     zakljucek = request.forms.zakljucek
-    if ime == '' or opis == '' or cena == '' or zakljucek == '':
+    if ime == '' or opis == '' or cena == '' or zakljucek == '': # v primeru, da je kakšno polje prazno vrne napako
         return rtemplate('dodaj_oglas.html', stanje = stanje, napaka = 3)
 
-    if not cena.isdigit():
-        return rtemplate('dodaj_oglas.html', stanje = stanje, napaka = 1)
+    if not cena.isdigit(): # isdigit pove ali se lahko string spremeni v neko številko
+        return rtemplate('dodaj_oglas.html', stanje = stanje, napaka = 1) # vrne napako če se nemore pretvorit cena v številko
     cena = int(cena)
 
-    try:
+    try: # če lahko pretvorimo čas ok drugače pa napaka neveljavnega datuma
         end = dt.datetime.strptime(zakljucek, '%d.%m.%Y %H:%M')
     except:
         return rtemplate('dodaj_oglas.html', stanje = stanje, napaka = 2)
 
-    if end < datetime.now():
+    if end < datetime.now(): # veljavnost časa je potekla zato vrne napako
         return rtemplate('dodaj_oglas.html', stanje = stanje, napaka = 2)
 
     data = odpri_json('podatki/oglasi.json')
@@ -320,14 +324,14 @@ def dodaj():
     for i in data:
         seznam.append(i['id'])
     try: 
-        naslednja_stevilka = max(seznam) + 1
+        naslednja_stevilka = max(seznam) + 1 # najdemo naslednjo številko
     except: naslednja_stevilka = 1
 
     nov_oglas = {"id":naslednja_stevilka, "ime":ime, "opis":opis, "prodajalec_id":stanje, "zakljucek":zakljucek, "zacetna_cena":cena}
 
-    dodaj_in_zapisi('podatki/oglasi.json', nov_oglas)
+    dodaj_in_zapisi('podatki/oglasi.json', nov_oglas) # zapišemo nov oglas
 
-    redirect('{0}uporabnik/'.format(ROOT))
+    redirect('{0}uporabnik/'.format(ROOT)) # vrne na stran uporabnika
 
 
 run(host='localhost', port=SERVER_PORT, reloader=RELOADER)
